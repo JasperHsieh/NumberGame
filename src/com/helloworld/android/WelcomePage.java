3ed55624391519c140f477ce3f9d6b04d19fc317
Jasper_Hsieh <Jasper_Hsieh@asus.com>
2015-08-18 11:49:33 +0800

    Modified game flow
    Add Fight mode and rival pin code dialog
    TODO: handle input rival's pin code and pair opponent

---
+++ src/com/helloworld/android/WelcomePage.java
@@ -0,0 +1,59 @@
+package com.numbergame.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+
+
+public class WelcomePage extends Activity {
+	
+	final private String TAG = "WelcomePage";
+	
+	Button level_btn;
+	Button fight_btn;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome);
+
+		level_btn = (Button) findViewById(R.id.level_mode);
+		fight_btn = (Button) findViewById(R.id.fight_mode);
+		
+		level_btn.setOnClickListener(mListener);
+		fight_btn.setOnClickListener(mListener);
+
+	}
+
+	private OnClickListener mListener = new OnClickListener(){
+		
+		@Override
+		public void onClick(View view){
+			
+			switch(view.getId()){
+				
+				case R.id.level_mode:
+					Log.d(TAG, "jasper level mode button");
+					break;
+				case R.id.fight_mode:
+					Log.d(TAG, "jasper fight mode button");
+					startFightModeActivity();
+					break;
+			}
+		}
+	};
+
+	private void startFightModeActivity(){
+		
+		Intent intent = new Intent(this, FightModeGame.class);
+		startActivity(intent);
+	}
+
+
+
+
+}
